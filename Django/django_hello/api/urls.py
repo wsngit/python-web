2ea7120a26cc3af8297ee1683da28cd5776ae5b6
Spirin Ivan <wensenmail@gmail.com>
2025-09-30 22:44:13 +0300

    Django

---
+++ Django/django_hello/api/urls.py
@@ -0,0 +1,13 @@
+# api/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hello/', views.hello_world),
+    path('greet/', views.hello_name),
+
+    path('hello/<str:name>/', views.hello),
+
+]
+
+
---
+++ Django/django_hello/api/views.py
@@ -0,0 +1,21 @@
+# api/views.py
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def hello_world(request):
+    return Response({"message": "Hello World!"})
+
+@api_view(['GET', 'POST'])
+@renderer_classes([JSONRenderer])  # Только JSON для этого view
+def hello_name(request):
+    if request.method == 'POST':
+        name = request.data.get('name', 'World')
+        return Response({"message": f"Hello, {name}!"})
+    else:
+        return Response({"message": "Send a POST request with your name!"})
+
+@api_view(['GET'])
+def hello(request, name):
+    return Response({"message": f"Hello {name}!"})
